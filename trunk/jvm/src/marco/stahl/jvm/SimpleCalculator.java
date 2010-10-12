3357ec0196996b138315d62155664dfbab5f6baf
shybyte <shybyte@a652cd76-7cbc-422a-877f-1316568ead3f>
2010-10-12 05:48:50 +0000

    Restructured

--- trunk/jvm/src/marco/stahl/jvm/SimpleCalculator.java
+++ trunk/jvm/src/marco/stahl/jvm/SimpleCalculator.java
@@ -1,15 +1,36 @@
 package marco.stahl.jvm;
 
 public class SimpleCalculator {
+
+	public static int pow(int b,int e){
+		int p = 1;
+		for(int i=0;i<e;i++) {
+			p=p*b;
+		}
+		return p;
+	} 
 	
-	public static int sum(int x){
+	public static int mul(int x,int y){
+		return x*y;
+	}
+	
+	public static int sign(int x){
+		if (x>0) {
+			return 1;
+		} else if (x<0) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+	
+	public static int inc(int x){
 		return x+1;
 	}
 	
     public static void main(String [] args)
     {
-        int x=1;
-        System.out.println(sum(x));
+        System.out.println("Result:"+pow(2,8));
     }
 
 }
