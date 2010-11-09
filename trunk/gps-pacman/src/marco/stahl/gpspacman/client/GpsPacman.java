b6d375b2cca639671a61de503e9cfda64049136d
shybyte <shybyte@a652cd76-7cbc-422a-877f-1316568ead3f>
2010-11-09 07:14:23 +0000

    added geo location

--- trunk/gps-pacman/src/marco/stahl/gpspacman/client/GpsPacman.java
+++ trunk/gps-pacman/src/marco/stahl/gpspacman/client/GpsPacman.java
@@ -9,6 +9,12 @@ import marco.stahl.gpspacman.client.georss.GeoRssFeedCallbackProxy;
 import marco.stahl.gpspacman.client.georss.GeoRssFeedItem;
 import marco.stahl.gpspacman.client.georss.JsCallback;
 
+import com.google.code.gwt.geolocation.client.Coordinates;
+import com.google.code.gwt.geolocation.client.Geolocation;
+import com.google.code.gwt.geolocation.client.Position;
+import com.google.code.gwt.geolocation.client.PositionCallback;
+import com.google.code.gwt.geolocation.client.PositionError;
+import com.google.code.gwt.geolocation.client.PositionOptions;
 import com.google.common.collect.Lists;
 import com.google.gwt.ajaxloader.client.AjaxLoader;
 import com.google.gwt.core.client.EntryPoint;
@@ -26,12 +32,15 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
-public class GpsPacman implements EntryPoint {
+public class GpsPacman implements EntryPoint, PositionCallback {
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
 	private MapWidget map;
+	private Marker playerPos;
+	private Geolocation geo;
+	private boolean isPositionListenerInstalled = false;
+	private PositionOptions positionOptions;
 
-	// GWT module entry point method.
 	@Override
 	public void onModuleLoad() {
 		AjaxLoader.init();
@@ -42,11 +51,27 @@ public class GpsPacman implements EntryPoint {
 					@Override
 					public void run() {
 						buildUi();
+						initGeoLocation();
 					}
 				}, null);
 			}
 		});
 
+	}
+
+	private void initGeoLocation() {
+		if (Geolocation.isSupported()) {
+			geo = Geolocation.getGeolocation();
+			if (geo != null) {
+				positionOptions = PositionOptions.create();
+				positionOptions.setEnableHighAccuracy(false);				
+				geo.getCurrentPosition(this,positionOptions);
+			} else {
+				Window.alert("Geo was null");
+			}
+		} else {
+			Window.alert("No Geo Position Support");
+		}
 	}
 
 	private void buildUi() {
@@ -62,20 +87,19 @@ public class GpsPacman implements EntryPoint {
 			// Add some controls for the zoom level
 			map.addControl(new LargeMapControl());
 
-			// Add a marker
-			Marker center = new Marker(auerPark);
-			map.addOverlay(center);
-			center.setImage("images/pacman.png");
+			playerPos = new Marker(auerPark);
+			map.addOverlay(playerPos);
+			playerPos.setImage("images/pacman.png");
 
-//			Polyline polyline = new Polyline(
-//					new LatLng[] { LatLng.newInstance(52.518490, 13.444229), LatLng
-//							.newInstance(52.518459, 13.444300), LatLng
-//							.newInstance(52.518429, 13.444290), LatLng
-//							.newInstance(52.518291, 13.445630), LatLng
-//							.newInstance(52.519058, 13.445140), LatLng
-//							.newInstance(52.518848, 13.444470), LatLng
-//							.newInstance(52.518490, 13.444232) });
-//			map.addOverlay(polyline);
+			// Polyline polyline = new Polyline(
+			// new LatLng[] { LatLng.newInstance(52.518490, 13.444229), LatLng
+			// .newInstance(52.518459, 13.444300), LatLng
+			// .newInstance(52.518429, 13.444290), LatLng
+			// .newInstance(52.518291, 13.445630), LatLng
+			// .newInstance(52.519058, 13.445140), LatLng
+			// .newInstance(52.518848, 13.444470), LatLng
+			// .newInstance(52.518490, 13.444232) });
+			// map.addOverlay(polyline);
 
 			flowPanel.add(map);
 		}
@@ -83,31 +107,30 @@ public class GpsPacman implements EntryPoint {
 
 		FlowPanel console = new FlowPanel();
 		flowPanel.add(console);
-		loadMap(new GeoRssFeedCallbackProxy(new JsCallback<GeoRssFeed>(){
-			
+		loadMap(new GeoRssFeedCallbackProxy(new JsCallback<GeoRssFeed>() {
+
 			@Override
 			public void onSuccess(GeoRssFeed feed) {
-				drawLines(feed);				
-				
+				drawLines(feed);
 			}
-			
+
 			@Override
 			public void onFailure(String caught) {
 				Window.alert(caught);
-				
+
 			}
 		}));
 	}
-	
+
 	private native void loadMap(GeoRssFeedCallbackProxy callback) /*-{
 		var feed = new $wnd.google.feeds.Feed("http://maps.google.com/maps/ms?ie=UTF8&hl=en&vps=1&jsv=290a&msa=0&output=georss&msid=100976889524593770371.0004935c45a9beae52c82");
 		feed.setResultFormat($wnd.google.feeds.Feed.MIXED_FORMAT);
 		feed.load(function(result) {
-		  if (!result.error) {          
-		      callback.@marco.stahl.gpspacman.client.georss.GeoRssFeedCallbackProxy::onSuccess(Lmarco/stahl/gpspacman/client/georss/GeoRssFeed;)(result.feed);
-		  } else {
-		  	callback.@marco.stahl.gpspacman.client.georss.GeoRssFeedCallbackProxy::onFailure(Ljava/lang/String;)(result.error);
-		  }
+		if (!result.error) {          
+		callback.@marco.stahl.gpspacman.client.georss.GeoRssFeedCallbackProxy::onSuccess(Lmarco/stahl/gpspacman/client/georss/GeoRssFeed;)(result.feed);
+		} else {
+		callback.@marco.stahl.gpspacman.client.georss.GeoRssFeedCallbackProxy::onFailure(Ljava/lang/String;)(result.error);
+		}
 		});
 	}-*/;
 
@@ -116,7 +139,7 @@ public class GpsPacman implements EntryPoint {
 		for (int i = 0; i < items.length(); i++) {
 			GeoRssFeedItem item = items.get(i);
 			GeoLine line = item.getLine();
-			if (line!=null) {
+			if (line != null) {
 				Polyline polyline = createGoogleMapsPolyline(line);
 				map.addOverlay(polyline);
 			}
@@ -128,8 +151,44 @@ public class GpsPacman implements EntryPoint {
 		LatLng[] linePoints = new LatLng[geoPoints.size()];
 		for (int i = 0; i < linePoints.length; i++) {
 			GeoPoint geoPoint = geoPoints.get(i);
-			linePoints[i] = LatLng.newInstance(geoPoint.getLat(), geoPoint.getLong()); 
+			linePoints[i] = LatLng.newInstance(geoPoint.getLat(),
+					geoPoint.getLong());
 		}
 		return new Polyline(linePoints);
+	}
+
+	@Override
+	public void onSuccess(Position position) {
+		if (map != null) {
+			Coordinates coords = position.getCoords();
+			playerPos.setLatLng(LatLng.newInstance(coords.getLatitude(),
+					coords.getLongitude()));
+		}
+		if (!isPositionListenerInstalled) {
+			geo.watchPosition(this,positionOptions);
+			isPositionListenerInstalled = true;
+		}
+	}
+
+	@Override
+	public void onFailure(PositionError error) {
+		String message = "";
+		switch (error.getCode()) {
+		case PositionError.UNKNOWN_ERROR:
+			message = "Unknown Error";
+			break;
+		case PositionError.PERMISSION_DENIED:
+			message = "Permission Denied";
+			break;
+		case PositionError.POSITION_UNAVAILABLE:
+			message = "Position Unavailable";
+			break;
+		case PositionError.TIMEOUT:
+			message = "Time-out";
+			break;
+		default:
+			message = "Unknown error code.";
+		}
+		Window.alert(message);
 	}
 }
