c838fa7a61f59457806a88011c6f73b6b5563eb7
shybyte <shybyte@a652cd76-7cbc-422a-877f-1316568ead3f>
2011-04-19 05:18:19 +0000

    Initial import play stack test.

---
+++ trunk/play-stack-test/app/controllers.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import play.mvc.Scope.Session
+import play._
+import play.mvc._
+import play.mvc.Scope.Session;
+
+object Application extends Controller {
+    
+    def index() = {
+    	val currentSession = Session.current()
+    	val userName = currentSession.get("user")
+    	val counter = currentSession.get("count")
+    	val newCounter:Int = if (counter == null) 1 else Integer.parseInt(counter)+1
+    	Session.current().put("count", newCounter)
+    	Template(userName)
+    }
+    
+    def logout() = {
+    	Session.current().clear()
+    	Template()
+    }
+    
+}
