34be548170bf4d54f51a9dc1b72e4186117c6749
shybyte <shybyte@a652cd76-7cbc-422a-877f-1316568ead3f>
2011-05-04 21:39:10 +0000

    Play stack test. Finished Facebook signed request decoding

--- play-stack-test/app/dogather/facebook/FacebookRequest.scala
+++ play-stack-test/app/dogather/facebook/FacebookRequest.scala
@@ -1,5 +1,11 @@
 package dogather.facebook
 
-case class FacebookRequest(user:FacebookUser,algorithm:String,issued_at:Long) {
-	def this() = this(new FacebookUser(),"",0);
+case class FacebookRequest(
+  user: FacebookUser, 
+  algorithm: String, 
+  issued_at: Long,
+  user_id: String = null,
+  oauth_token:String = null,
+  expires: Long = 0) {
+  def this() = this(new FacebookUser(), "", 0);
 }
--- play-stack-test/test/dogather/facebook/FacebookUtilsTest.scala
+++ play-stack-test/test/dogather/facebook/FacebookUtilsTest.scala
@@ -15,9 +15,9 @@ class FacebookUtilsTest extends AssertionsForJUnit {
   def verifyEasy() {
     val data = "dYL9zkPDedL3De4s1pnVYLZIKNaIHp0wMxPGZyU9Ssk.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEzMDMzNzY0MDAsImlzc3VlZF9hdCI6MTMwMzM3MTIzMSwib2F1dGhfdG9rZW4iOiIxODc3NzQ2NzQ2MDEyNTV8Mi5nY3ZWd2xtaXE4aGNNbjM0dndPZkV3X18uMzYwMC4xMzAzMzc2NDAwLjEtMTQ4MjIwMTA3OXxUbVNTM0VhOXdHVWNsYlE5R2d1b1FIU1ZjRnMiLCJ1c2VyIjp7ImNvdW50cnkiOiJkZSIsImxvY2FsZSI6ImRlX0RFIiwiYWdlIjp7Im1pbiI6MjF9fSwidXNlcl9pZCI6IjE0ODIyMDEwNzkifQ";
     val result = FacebookUtils.decodeSignedRequest(data);
-    assertEquals(new FacebookRequest(new FacebookUser("de_DE", "de"),
-      "HMAC-SHA256", 1303371231), result);
-    //    println(result);
+    println(result);
+    assertEquals(FacebookRequest(FacebookUser("de_DE", "de"),
+      "HMAC-SHA256", 1303371231, "1482201079", "187774674601255|2.gcvVwlmiq8hcMn34vwOfEw__.3600.1303376400.1-1482201079|TmSS3Ea9wGUclbQ9GguoQHSVcFs", 1303376400), result);
   }
 
 }
